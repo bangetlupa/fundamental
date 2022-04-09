11de285615ee1af62f8465df6bc5a72b9ec4ebbb
HENDY PRIMA <hendyprima84@gmail.com>
2022-04-10 03:15:25 +0700

    percabangan

--- percabangan.py
+++ percabangan.py
@@ -1,17 +1,23 @@
 #percabangan
-jumlah_susu = 1
-jumlah_telur = 1
+print('mom said:"please go to the shop and buy 1 bottle of milk, if they have eggs,bring 6')
+jumlah_susu = 10
+jumlah_telur = 4
 print(f'jumlah botol susu {jumlah_susu} btl')
 print(f'jumlah telur {jumlah_telur} butir')
 
 if jumlah_susu > 0 :
-    print('budi cek harganya,dan uangnya cukup')
-    if jumlah_telur > 3:
-        print('budi membeli 1 botol susu')
-    else:
-        print('budi membeli 6 botol susu')
+    print('budi cek harga susu,dan uangnya cukup')
+if jumlah_susu > 0 :
+    print('budi membeli 1 botol susu')
 else:
-    print('budi tidak jadi beli susu')
+    print('tidak ada susu')
+
+if jumlah_telur > 5:
+    print('budi cek harga telur dan uangnya cukup')
+if jumlah_telur > 5:
+    print('budi membeli 6 butir telur')
+else:
+    print('tidak ada telur')
 
 #mom said:"please go to the shop and buy 1 bottle of milk, if they have eggs,bring 6
 #I came back with 6 bottle of milk
