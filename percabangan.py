f12b919875e4dec45c9f7b6492ebdbda238163ae
HENDY PRIMA <hendyprima84@gmail.com>
2022-04-10 02:23:09 +0700

    calculator

---
+++ percabangan.py
@@ -0,0 +1,24 @@
+#percabangan
+jumlah_susu = 1
+jumlah_telur = 1
+print(f'jumlah botol susu {jumlah_susu} btl')
+print(f'jumlah telur {jumlah_telur} butir')
+
+if jumlah_susu > 0 :
+    print('budi cek harganya,dan uangnya cukup')
+    if jumlah_telur > 0:
+        print('budi membeli 1 botol susu')
+    else:
+        print('budi membeli 6 botol susu')
+else:
+    print('budi tidak jadi beli susu')
+
+#mom said:"please go to the shop and buy 1 bottle of milk, if they have eggs,bring 6
+#I came back with 6 bottle of milk
+#mom said : 'why the hell did you buy 6 bottle of milk?
+#I said: "because they had eggg"
+
+
+
+
+
