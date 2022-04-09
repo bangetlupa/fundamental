ff9881e76e06beaabf102024dc56ba8e26f1e436
HENDY PRIMA <hendyprima84@gmail.com>
2022-04-10 02:30:25 +0700

    percabangan

--- percabangan.py
+++ percabangan.py
@@ -6,7 +6,7 @@ print(f'jumlah telur {jumlah_telur} butir')
 
 if jumlah_susu > 0 :
     print('budi cek harganya,dan uangnya cukup')
-    if jumlah_telur > 0:
+    if jumlah_telur > 3:
         print('budi membeli 1 botol susu')
     else:
         print('budi membeli 6 botol susu')
