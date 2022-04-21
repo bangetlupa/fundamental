de96a2dd48a6c9715c1119c1026db379e43c97ca
HENDY PRIMA <hendyprima84@gmail.com>
2022-04-21 23:23:47 +0700

    update 21/04

--- calculator.py
+++ calculator.py
@@ -1,27 +1,34 @@
 def tambah (x,y):
     return x + y
-def kurang(x, y):
+def kurang (x,y):
     return x - y
-def kali(x, y):
-    return x * y
-def bagi(x, y):
-    return x / y
-print("Select operation.");print("1.tambah");print("2.kurang");print("3.kali");print("4.bagi")
+def kali (x,y):
+    return x*y
+def bagi (x,y):
+    return x/y
+
+print('pilihlah operator');print('1.tambah 2.kurang 3.kali 4.bagi')
+
 while True:
-    choice = input("tentukan operator(1/2/3/4): ")
-    if choice in ('1', '2', '3', '4'):
-        num1 = float(input("Enter first number: "))
-        num2 = float(input("Enter second number: "))
-        if choice == '1':
-            print(num1, "+", num2, "=", tambah(num1, num2))
-        elif choice == '2':
-            print(num1, "-", num2, "=", kurang(num1, num2))
-        elif choice == '3':
-            print(num1, "*", num2, "=", kali(num1, num2))
-        elif choice == '4':
-            print(num1, "/", num2, "=", bagi(num1, num2))
+    operator = input('pilih operator:')
+    if operator in ('1','2','3','4'):
+        num1 = float(input(':'))
+        num2 = float(input(':'))
+        if operator == '1':
+            print(num1,'+',num2,'=',tambah(num1,num2))
+        elif operator == '2':
+            print(num1, '-', num2, '=', kurang(num1, num2))
+        elif operator == '3':
+            print(num1, '*', num2, '=', kali(num1, num2))
+        elif operator == '4':
+            print(num1, ':', num2, '=', bagi(num1, num2))
+        selesai = input("apakah mau terus jika tidak pilih 'no': ")
+        if selesai == 'no':
+            break
+
     else:
-        print("Invalid Input")
+        print('operator hanya 1,2.3.4')
+
 
 
 
