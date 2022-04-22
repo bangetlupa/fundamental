c1f1801067bcbcd614cbb9790a25f0629a20b7c4
HENDY PRIMA <hendyprima84@gmail.com>
2022-04-22 23:55:49 +0700

    update 22/04 23:17

--- 3. type data list.py
+++ 3. type data list.py
@@ -2,13 +2,6 @@
 # 'pop' membuang data paling ujung
 # 'len'
 
-'''
-
-                            Online Python Interpreter.
-                Code, Compile, Run and Debug python program online.
-Write your code in this editor and press "Run" button to execute it.
-
-'''
 
 gorengan = ['tahu', 'ubi', 'bakwan', 'risol']
 print('tampilkan variable gorengan')
