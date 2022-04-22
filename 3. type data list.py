c1aaa15da3f162638b956a5424d20b0df89cb0a0
HENDY PRIMA <hendyprima84@gmail.com>
2022-04-22 23:41:34 +0700

    update 22/04 23:17

--- 3. type data list.py
+++ 3. type data list.py
@@ -1,4 +1,46 @@
-gorengan = ('tahu,cireng,tempe,bakwan')
-print (gorengan)
+# 'append' menambahkan data di paling ujung
+# 'pop' membuang data paling ujung
+# 'len'
+
+'''
+
+                            Online Python Interpreter.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+gorengan = ['tahu', 'ubi', 'bakwan', 'risol']
+print('tampilkan variable gorengan')
+print(gorengan)
+
+print('\nproses semua dengan for in')
+for i in gorengan:
+    print(i)
+
+print('\ntampilkan isi gorengan pada indeks tertentu')
+print(gorengan[0])
+print(gorengan[2])
+
+print('\ntampilkan dengan for in range')
+for i in range(0, len(gorengan)):
+    print(gorengan[i])
+
+gorengan = [1, 'tahu', 121, 42]
+print('\ntampilkan dengan for in range,dimana tipe data berbeda')
+for i in range(0, len(gorengan)):
+    print(gorengan[i])
+
+print('kembalikan list gorengan')
+gorengan = ['tahu', 'ubi', 'bakwan', 'risol']
+print('tambahan 1 gorengan baru')
+gorengan.append('pisang')  # menambahkan isi list
+print(gorengan)
+
+# mylist = "Hellobro"
+# x = len(gorengan)
+# print(x)
+
+
 
 
